ba7e77f1f3045621480b5ee19a917c6d871d70f5
yan petcha <yannick.tchape@gmail.com>
2019-05-06 11:18:52 +0200

    projet Open de messagerie instantanée

---
+++ src/main/java/com/open/messengerOpen/messengerOpen/MessengerOpenApplication.java
@@ -0,0 +1,13 @@
+package com.open.messengerOpen.messengerOpen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerOpenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerOpenApplication.class, args);
+	}
+
+}
